7a0971b6adf75c9de58198deef9bfc4f626832c1
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-13 15:25:48 +0100

    Initial project added

---
+++ pet-clinic/src/main/java/haydende/sfgstylepetclinic/SfgStylePetclinicApplication.java
@@ -0,0 +1,13 @@
+package haydende.sfgstylepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgStylePetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgStylePetclinicApplication.class, args);
+    }
+
+}
---
+++ pet-clinic/src/test/java/haydende/sfgstylepetclinic/SfgStylePetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package haydende.sfgstylepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgStylePetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
