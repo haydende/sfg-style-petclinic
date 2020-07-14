8a3270e1bd0253b5a070c351675b3e11844ef680
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-14 13:12:10 +0100

    Refactored the Service Interfaces to use a common Interface (CrudService) so that they only need to contain methods specific to their purpose.
    Closes #10

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/services/PetService.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package haydende.sfgstylepetclinic.services;
 
 import haydende.sfgstylepetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
