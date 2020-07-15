e22246ea0e2af6f906014678ca923bdccb8415b0
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-15 16:04:37 +0100

    Added Pet Index Page (under resources/templates/pet/index.html) and Controller (under Java/haydende.sfgstylepetclinic.controllers/PetController.java).
    Closes #12

---
+++ pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/controllers/PetController.java
@@ -0,0 +1,15 @@
+package haydende.sfgstylepetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/pets")
+@Controller
+public class PetController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String listPets() {
+        return "pets/index";
+    }
+
+}
