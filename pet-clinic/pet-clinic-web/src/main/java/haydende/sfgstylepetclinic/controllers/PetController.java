6bb3df799e5f3dd8a1aba098c2d304e48c3edf5c
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-16 09:10:46 +0100

    (Forgot to add in previous commits) Added Pets and Vets lists to their respective index pages
    closes #23 and closes #14

--- pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/controllers/PetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/controllers/PetController.java
@@ -1,14 +1,23 @@
 package haydende.sfgstylepetclinic.controllers;
 
+import haydende.sfgstylepetclinic.services.PetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/pets")
 @Controller
 public class PetController {
 
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
     @RequestMapping({"", "/", "index", "index.html"})
-    public String listPets() {
+    public String listPets(Model model) {
+        model.addAttribute("pets", petService.findAll());
         return "pets/index";
     }
 
--- pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/controllers/VetController.java
+++ pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package haydende.sfgstylepetclinic.controllers;
 
+import haydende.sfgstylepetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
