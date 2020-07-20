dc1e9fec6f178af49b5ab020caabd11a201e003f
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-20 17:16:47 +0100

    Enhanced Owners with contact info and pets for the startup process.
    closes #33

--- pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package haydende.sfgstylepetclinic.bootstrap;
 
 import haydende.sfgstylepetclinic.model.Owner;
+import haydende.sfgstylepetclinic.model.Pet;
 import haydende.sfgstylepetclinic.model.PetType;
 import haydende.sfgstylepetclinic.model.Vet;
 import haydende.sfgstylepetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import haydende.sfgstylepetclinic.services.PetTypeService;
 import haydende.sfgstylepetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -39,13 +42,29 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("First-Name");
         owner1.setLastName("Last-Name");
+        owner1.setAddress("123 Street");
+        owner1.setCity("Citytown");
+        owner1.setTelephone("123456789");
 
+        Pet owner1Pet = new Pet();
+        owner1Pet.setName("Owner1Dog");
+        owner1Pet.setPetType(dog);
+        owner1Pet.setOwner(owner1);
+        owner1Pet.setBirthDate(LocalDate.of(1999, 07, 12));
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Nirst-Fame");
         owner2.setLastName("Nast-Lame");
+        owner2.setAddress("123 Street");
+        owner2.setCity("Citytown");
+        owner2.setTelephone("123456789");
 
+        Pet owner2Pet = new Pet();
+        owner2Pet.setName("Owner2Cat");
+        owner2Pet.setPetType(cat);
+        owner2Pet.setOwner(owner2);
+        owner2Pet.setBirthDate(LocalDate.of(2005, 12, 16));
         ownerService.save(owner2);
 
         System.out.println("Loaded the owners...");
