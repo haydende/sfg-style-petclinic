7a530e7fe69d02a9aa8a0d31fc0f88e7b4684587
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-20 16:00:49 +0100

    Added PetType instances to the DataLoader.
    Closes #32

--- pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/bootstrap/DataLoader.java
+++ pet-clinic/pet-clinic-web/src/main/java/haydende/sfgstylepetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package haydende.sfgstylepetclinic.bootstrap;
 
 import haydende.sfgstylepetclinic.model.Owner;
+import haydende.sfgstylepetclinic.model.PetType;
 import haydende.sfgstylepetclinic.model.Vet;
 import haydende.sfgstylepetclinic.services.OwnerService;
+import haydende.sfgstylepetclinic.services.PetTypeService;
 import haydende.sfgstylepetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,16 +13,29 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
-
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService,
+                      PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        dog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        cat = petTypeService.save(cat);
+
+        System.out.println("Loaded PetTypes...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("First-Name");
         owner1.setLastName("Last-Name");
