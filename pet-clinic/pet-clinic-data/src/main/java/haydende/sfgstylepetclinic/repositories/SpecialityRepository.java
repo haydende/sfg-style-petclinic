8edd7568d3565eca647db297957565173c3c7345
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-22 17:56:52 +0100

    Added Speciality JPA Service class and updated methods in the SpecialityRepository interface.
    Closes #44

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/repositories/SpecialityRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/repositories/SpecialityRepository.java
@@ -4,4 +4,11 @@ import haydende.sfgstylepetclinic.model.Speciality;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+
+    Speciality save(Speciality speciality);
+
+    void delete(Speciality speciality);
+
+    void deleteById(Long id);
+
 }
