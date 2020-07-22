147cb1ad1d6e1cfa9114e074e20bf7bf1b2e61e8
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-22 16:40:31 +0100

    Added Owner JPA Service class and updated the methods in the OwnerRepository.
    Closes #40

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/repositories/OwnerRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/repositories/OwnerRepository.java
@@ -3,5 +3,19 @@ package haydende.sfgstylepetclinic.repositories;
 import haydende.sfgstylepetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastname);
+
+    Set<Owner> findAll();
+
+    Owner save(Owner owner);
+
+    void delete(Owner owner);
+
+    void deleteById(Long id);
+
+    void deleteAll();
 }
---
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/services/springdatajpa/OwnerSDjpaService.java
@@ -0,0 +1,58 @@
+package haydende.sfgstylepetclinic.services.springdatajpa;
+
+import haydende.sfgstylepetclinic.model.Owner;
+import haydende.sfgstylepetclinic.repositories.OwnerRepository;
+import haydende.sfgstylepetclinic.repositories.PetRepository;
+import haydende.sfgstylepetclinic.repositories.PetTypeRepository;
+import haydende.sfgstylepetclinic.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDjpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDjpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return ownerRepository.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        // get Optional value then...
+        // if value is present, return it. If not, return null
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
