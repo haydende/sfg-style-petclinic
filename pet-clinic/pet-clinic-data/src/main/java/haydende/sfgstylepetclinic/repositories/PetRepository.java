108a307bfeded52777e000c34664f5bc9568b177
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-22 17:39:07 +0100

    Added Pet JPA Service class and updated the methods in the PetRepository.
    Closes #42
    Closes #43

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/repositories/PetRepository.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/repositories/PetRepository.java
@@ -4,4 +4,10 @@ import haydende.sfgstylepetclinic.model.Pet;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PetRepository extends CrudRepository<Pet, Long> {
+
+    Pet save(Pet pet);
+
+    void delete(Pet pet);
+
+    void deleteById(Long id);
 }
