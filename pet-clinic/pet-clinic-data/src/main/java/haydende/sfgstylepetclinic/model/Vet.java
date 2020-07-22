28ec2368199f39e0c7e395e2a2d6d243be1c9387
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-22 15:25:08 +0100

    Added JPA annotations to the Vet and Speciality classes.
    Closes #37

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/model/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/model/Vet.java
@@ -1,10 +1,22 @@
 package haydende.sfgstylepetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        // Define the name of the Join Table
+        name = "vet_specialities",
+        // Column from this table used in the join table
+        joinColumns = @JoinColumn(name = "vet_id"),
+        // Column from the joining table used in the join table
+        inverseJoinColumns = @JoinColumn(name = "speciality")
+    )
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
