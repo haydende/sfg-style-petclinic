897ad10ce87086f5779085d9dc6f0fb0041bce5b
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-22 14:05:24 +0100

    Added the @MappedSuperclass annotation to the BaseEntity class, so it has been completely implemented.
    Closes #35

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/model/BaseEntity.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package haydende.sfgstylepetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+// Tell JPA that this class will be used for inheritance
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
