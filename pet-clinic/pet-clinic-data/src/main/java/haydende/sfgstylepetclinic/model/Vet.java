b2050e504d75db46c2efb2377ed1b23b7319d269
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-23 14:14:02 +0100

    Refactored source with Lambok annotations
    Closes #53

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/model/Vet.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package haydende.sfgstylepetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -19,11 +26,4 @@ public class Vet extends Person{
     )
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
