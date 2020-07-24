c69f85190ff846df7eb77dd3de0ebff9b7bae1eb
Hayden Eastwell <42878077+haydende@users.noreply.github.com>
2020-07-24 16:20:30 +0100

    Added implementation for OwnerMapService::findByLastName.

--- pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/services/map/OwnerMapService.java
+++ pet-clinic/pet-clinic-data/src/main/java/haydende/sfgstylepetclinic/services/map/OwnerMapService.java
@@ -79,6 +79,10 @@ public class OwnerMapService extends AbstractMapService<Owner, Long>
 
     @Override
     public Owner findByLastName(String lastName) {
-        return null;
+        return this.findAll()
+            .stream()
+            .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+            .findFirst()
+            .orElse(null);
     }
 }
